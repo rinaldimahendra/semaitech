f3c9250782b2f33c6fde454d3a868ebfdc2abe34
hklrzki <haikalrizki38@gmail.com>
2022-06-25 22:44:10 +0700

    Tentang Perusahaan (tampilan dan fungsi)

--- application/views/v_tentangperusahaan.php
+++ application/views/v_tentangperusahaan.php
@@ -2,64 +2,44 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 $this->load->view('template_user/header');
 ?>
-        
-<section class="bg0 p-t-75 p-b-120">
+
+<section class="bg-img1 txt-center p-lr-15 p-tb-92">
     <div class="container">
-        <div class="row p-b-148">
-            <div class="col-md-7 col-lg-8">
-                    <h3 style="text-align:right"class="mtext-111 cl2 p-b-16">
-                        
-                    Tentang <?=htmlspecialchars_decode($profil_perusahaan['nama']); ?>
-                    </h3>
+    <img src="<?= $profil_perusahaan['logo']; ?>" alt="" style="width:190px">
+        <h2 class="ltext-105 cl0 txt-center p-b-60 "style="color:#000000;">
+			Tentang <?=htmlspecialchars_decode($profil_perusahaan['nama']); ?>
+		</h2>
+        <p class="stext-113 cl2 p-b-26">
+        <?=htmlspecialchars_decode($profil_perusahaan['tentang']); ?>
+        </p>
 
-                    <h3 class="mtext-111 cl2 p-b-16">
-                        TENTANG
-                    </h3>
-                    <p class="stext-113 cl6 p-b-26">
-                    <?=htmlspecialchars_decode($profil_perusahaan['tentang']); ?>
-                    </p>
+        <h3 class="mtext-111 cl2 p-b-16">
+            VISI
+        </h3>
+        <p class="stext-113 cl2 p-b-26">
+        <?=htmlspecialchars_decode($profil_perusahaan['visi']); ?>
+        </p>
 
-                    <h3 class="mtext-111 cl2 p-b-16">
-                        VISI
-                    </h3>
-                    <p class="stext-113 cl6 p-b-26">
-                    <?=htmlspecialchars_decode($profil_perusahaan['visi']); ?>
-                    </p>
+        <h3 class="mtext-111 cl2 p-t-26 p-b-16">
+            MISI
+        </h3>
+        <p class="stext-113 cl2 p-b-26">
+        <?=htmlspecialchars_decode($profil_perusahaan['misi']); ?>
+        </p>
 
-                    <h3 class="mtext-111 cl2 p-b-16">
-                        MISI
-                    </h3>
-                    <p class="stext-113 cl6 p-b-26">
-                    <?=htmlspecialchars_decode($profil_perusahaan['misi']); ?>
-                    </p>
 
-                    <h3 class="mtext-111 cl2 p-b-16">
-                        KONTAK
-                    </h3>
-
-                    <!-- <div class="col-11 col-md-5 col-lg-4 m-lr-auto">
-					<div class="how-bor1 ">
-						<div class="hov-img0">
-							<img src="images/rumput.jpg" alt="IMG">
-						</div>
-					</div> -->
-                    
-                    <p class="stext-113 cl6 p-b-26">
-                    Nomor Telepon: <?=$profil_perusahaan['nomor_kontak']; ?>
-                    E-mail: <?=$profil_perusahaan['email']; ?>
-                    Alamat: <?=$profil_perusahaan['alamat']; ?>
-
-                    </p>
+        <h3 class="mtext-111 cl2 p-b-16">
+            KONTAK
+        </h3>
         
-                </div>
-            </div>
-        </div>
-    </div> 
-</section>	
+        <p class="stext-113 cl2 p-b-26">
+        Nomor Telepon: <?=$profil_perusahaan['nomor_kontak']; ?> <br>
+        E-mail: <?=$profil_perusahaan['email']; ?> <br>
+        Alamat: <?=$profil_perusahaan['alamat']; ?>
 
-        
-      <!-- </h1>
+        </p>
     </div>
-    <div class="card">
-      <div class="card-header"> -->
+    
+</section>	
+        	
 <?php $this->load->view('template_user/footer'); ?>
